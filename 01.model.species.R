2065e1a0c39144317ae02b66ef976813f58df3e2
Jeremy VanDerWal <jjvanderwal@gmail.com>
2012-07-31 10:20:29 +0000

    git-svn-id: http://sdmcode.googlecode.com/svn@405 89c6b09d-80c0-cc51-af33-c44c18b3b9a7

--- 01.model.species.R
+++ 01.model.species.R
@@ -2,63 +2,48 @@
 #GNU General Public License .. feel free to use / distribute ... no warranties
 
 ################################################################################
-library(SDMTools); library(maptools) #load the necessary libraries
+library(SDMTools) #load the necessary libraries
 
 wd = '~/working/NARP_birds/'; setwd(wd) #define and set theworking directory
 climate.dir = '~/Climate/CIAS/Australia/5km/baseline.76to05/bioclim/' #define the climate directory
 
-polys = readShapePoly('raw.data/unzipped/TaxonPolys_Dissolve.shp') #read in the polygon files
-occur = read.csv('raw.data/observations.csv.gz',as.is=TRUE) #read in the observations
-occur = occur[which(occur$Positional_Accuracy<=20000),] #keep only things with <20km accuracy
-occur = occur[which(occur$rating %in% c(0,1,5)),] #keep only ratings of 0,1,5
+occur = read.csv('raw.data/ClimData.csv.gz',as.is=TRUE,header=FALSE) #read in the observations
+colnames(occur) = c('ClimID','Latitude','Longitude','Positional_Accuracy','Start_Date','Year') #define the column names
+occur = occur[which(occur$Positional_Accuracy<=25000),] #keep only things with <20km accuracy
+bkgd = read.csv('raw.data/ClimSurveys.csv.gz',as.is=TRUE) #read in the background data
+
+occur$Latitude = round(occur$Latitude/0.05)*0.05; bkgd$Latitude = round(bkgd$Latitude/0.05)*0.05 #round latitude to 0.05 degrees
+occur$Longitude = round(occur$Longitude/0.05)*0.05; bkgd$Longitude = round(bkgd$Longitude/0.05)*0.05 #round Longitude to 0.05 degrees
+
+occur = occur[,c('ClimID','Latitude','Longitude')] #keep only columns of interest
+bkgd$ClimID='bkgd'; bkgd = bkgd[,c('ClimID','Latitude','Longitude')] #keep only columns of interest
+nrow(occur); occur = unique(occur); nrow(occur) #keep only unique info
+nrow(bkgd); bkgd = unique(bkgd); nrow(bkgd) #keep only unique info
 
 clim.vars = paste('bioclim_',sprintf('%02i',c(1,4,5,6,12,15,16,17)),sep='') #define the climate variables of interest
 for (clim.var in clim.vars) { cat(clim.var,'\n') #cycle through each of hte climate variables
-	occur[,clim.var] = extract.data(cbind(occur$Lon,occur$Lat), read.asc.gz(paste(climate.dir,clim.var,'.asc.gz',sep=''))) #append the climate data
+	occur[,clim.var] = extract.data(cbind(occur$Longitude,occur$Latitude), read.asc.gz(paste(climate.dir,clim.var,'.asc.gz',sep=''))) #append the climate data
+	bkgd[,clim.var] = extract.data(cbind(bkgd$Longitude,bkgd$Latitude), read.asc.gz(paste(climate.dir,clim.var,'.asc.gz',sep=''))) #append the climate data
 }
-occur = na.omit(occur[,c('SpNo','Lat','Lon',colnames(occur)[grep('bioclim',colnames(occur))])]) #reorder the occur
-bkgd = occur; bkgd$SpNo = 'bkgd'; bkgd = unique(bkgd) #define the background
+bkgd = na.omit(bkgd); occur = na.omit(occur) #remove missing data
 write.csv(bkgd,'target_group_bkgd.csv',row.names=FALSE); rm(bkgd) #write out the file and remove it from memory
 
-SpNos = sort(na.omit(as.numeric(unique(occur$SpNo)))) #get the unique species numbers
+occur$ClimID = gsub('br&nb','br_nb',occur$ClimID)
+SpNos = unique(occur$ClimID) #get the unique species numbers
 
-for (SpNo in SpNos) {  #cycle through each of the species numbers
-	if (SpNo %in% polys@data$SPNO) { cat(SpNo,'\n')
-		toccur = occur[occur$SpNo==SpNo,]; coordinates(toccur) = ~Lon+Lat #get a subset of the data
-		tpolys = polys[which(polys@data$SPNO==SpNo),] #get the associated polygons
-		tout = overlay(toccur,tpolys) #get what polygons a point falls within
-		tdata = na.omit(data.frame(toccur,tpolys@data[tout,])) #append the associated polygon data with the occurence, omitting anything that fell outside a polygon
-		tdata = tdata[which(tdata$RANGE_T %in% c('historic','core')),] #remove suspect, vagrant, escaped, irruptive and introduced records
-		if (nrow(tdata)>0) {
-			out = data.frame(spp = paste('u',SpNo,sep=''),tdata[,c('Lon','Lat',clim.vars)]) #create the FUll species model
-			if (length(which(tdata$BR_RNGE_T=='breeding'))>0) out = rbind(out,data.frame(spp = paste('u',SpNo,'_breed',sep=''),tdata[which(tdata$BR_RNGE_T=='breeding'),c('Lon','Lat',clim.vars)])) #append the breeding range
-			if (length(which(tdata$BR_RNGE_T=='non-breeding'))>0) out = rbind(out,data.frame(spp = paste('u',SpNo,'_nonbreed',sep=''),tdata[which(tdata$BR_RNGE_T=='non-breeding'),c('Lon','Lat',clim.vars)])) #append the breeding range
-
-			subspps = as.vector(tdata$TAXONID); subspps = gsub('u','',subspps); subspps = gsub(SpNo,'',subspps) #get teh subspecies designations
-			
-			for (subspp in unique(subspps)) { # cycle through each of the subspecies
-				if (nchar(subspp)>0) {
-					rois = grep(subspp,subspps) #get a list of the subspecies and hybrid zones
-					if (length(rois)>0) out = rbind(out,data.frame(spp = paste('u',SpNo,subspp,sep=''),tdata[rois,c('Lon','Lat',clim.vars)])) #get the core of the species
-				}
-			}
-			out$spp = as.character(out$spp) #convert from factor to character
-
-			print(unique(out$spp))
-			
-			spp.dir = paste(wd,'models/',SpNo,'/',sep='') #define the species directory
-			dir.create(paste(spp.dir,'output',sep=''),recursive=TRUE) #create the species directory
-			write.csv(out,paste(spp.dir,'occur.csv',sep=''),row.names=FALSE) #write out the file and remove it from memory
-			zz = file(paste(spp.dir,'01.create.model.sh',sep=''),'w') #create the shell script to run the maxent model
-				cat('#!/bin/bash\n',file=zz)
-				cat('cd $PBS_O_WORKDIR\n',file=zz)
-				cat('module load java\n',file=zz)
-				cat('java -mx1024m -jar ',wd,'maxent.jar -e ',wd,'target_group_bkgd.csv -s occur.csv -o output nothreshold nowarnings novisible replicates=10 nooutputgrids -r -a \n',sep="",file=zz)
-				cat('cp -af output/maxentResults.csv output/maxentResults.crossvalide.csv\n',file=zz)
-				cat('java -mx1024m -jar ',wd,'maxent.jar -e ',wd,'target_group_bkgd.csv -s occur.csv -o output nothreshold nowarnings novisible nowriteclampgrid nowritemess writeplotdata -P -J -r -a \n',sep="",file=zz)
-			close(zz) 
-			setwd(spp.dir); system(paste('qsub -m n -N ',SpNo,' 01.create.model.sh',sep='')); setwd(wd) #submit the script	
-		}
-	}
+for (SpNo in SpNos) {  cat(SpNo,'\n')#cycle through each of the species numbers
+	toccur = occur[occur$ClimID==SpNo,] #get the observations for the species
+	spp.dir = paste(wd,'models/',SpNo,'/',sep='') #define the species directory
+	dir.create(paste(spp.dir,'output',sep=''),recursive=TRUE) #create the species directory
+	write.csv(toccur,paste(spp.dir,'occur.csv',sep=''),row.names=FALSE) #write out the file and remove it from memory
+	zz = file(paste(spp.dir,'01.create.model.sh',sep=''),'w') #create the shell script to run the maxent model
+		cat('#!/bin/bash\n',file=zz)
+		cat('cd $PBS_O_WORKDIR\n',file=zz)
+		cat('module load java\n',file=zz)
+		cat('java -mx1024m -jar ',wd,'maxent.jar -e ',wd,'target_group_bkgd.csv -s occur.csv -o output nothreshold nowarnings novisible replicates=10 nooutputgrids -r -a \n',sep="",file=zz)
+		cat('cp -af output/maxentResults.csv output/maxentResults.crossvalide.csv\n',file=zz)
+		cat('java -mx1024m -jar ',wd,'maxent.jar -e ',wd,'target_group_bkgd.csv -s occur.csv -o output nothreshold nowarnings novisible nowriteclampgrid nowritemess writeplotdata -P -J -r -a \n',sep="",file=zz)
+	close(zz) 
+	setwd(spp.dir); system(paste('qsub -m n -N ',SpNo,' 01.create.model.sh',sep='')); setwd(wd) #submit the script	
 }
 
--- 02.project.distributions.R
+++ 02.project.distributions.R
@@ -10,28 +10,23 @@ maxent.jar = "~/working/NARP_birds/maxent.jar"
 ################################################################################
 #list the projections, cycle thorugh them and project the models onto them
 proj.list = list.files(mxe.dir) #list the projections
-proj.list = c('1990',proj.list[grep('2085',proj.list)]) #subset it to simply current and 2080 data
+proj.list = c('1990',proj.list[intersect(grep('2085',proj.list),grep('RCP',proj.list))]) #subset it to simply current and 2080 data
 
 species = list.files(work.dir) #get a list of species
 
 #cycle through each of the species
 for (spp in species) {
 	spp.dir = paste(work.dir,spp,'/',sep=''); setwd(spp.dir) #set the working directory to the species directory
-	spplist = read.csv('output/maxentResults.csv',as.is=TRUE)$Species #get a full listing of species and subspecies run
-	
-	##create the sh file
-	zz = file('02.project.models.sh','w')
+	zz = file('02.project.models.sh','w') ##create the sh file
 		cat('#!/bin/bash\n',file=zz)
 		cat('cd $PBS_O_WORKDIR\n',file=zz)
 		cat('module load java\n',file=zz)
-		for (subspp in spplist) { #cycle through each of the subspecies
-			dir.create(paste('output/ascii/',subspp,'/',sep=''),recursive=TRUE) #create the output directory for all maps
-			#cycle through the projections
-			for (tproj in proj.list) cat('java -mx1024m -cp ',maxent.jar,' density.Project ',spp.dir,'output/',subspp,'.lambdas ',mxe.dir,tproj,' ',spp.dir,'output/ascii/',subspp,'/',tproj,'.asc fadebyclamping nowriteclampgrid\n',sep="",file=zz)
-			cat('gzip ',spp.dir,'output/ascii/',subspp,'/*asc\n',sep='',file=zz)
-		}
+		dir.create('output/ascii/',recursive=TRUE) #create the output directory for all maps
+		#cycle through the projections
+		for (tproj in proj.list) cat('java -mx1024m -cp ',maxent.jar,' density.Project ',spp.dir,'output/',spp,'.lambdas ',mxe.dir,tproj,' ',spp.dir,'output/ascii/',tproj,'.asc fadebyclamping nowriteclampgrid\n',sep="",file=zz)
+		cat('gzip ',spp.dir,'output/ascii/*asc\n',sep='',file=zz)
 	close(zz) 
 
 	#submit the script
-	system('qsub -m n 02.project.models.sh',wait = FALSE)
+	system('qsub -m n 02.project.models.sh')
 }
